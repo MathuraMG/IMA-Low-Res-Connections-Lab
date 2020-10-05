931bae286c030180c9f005fd77c202a27870e89f
Craig <craig@Craigs-MBP.localdomain>
2020-10-05 09:07:32 -0700

    Updated var declaration to const

--- Week_5/p5 Canvas in a Div/sketch.js
+++ Week_5/p5 Canvas in a Div/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 	//Create the canvas and save it to a variable;
-	var myCanvas = createCanvas(400,300);
+	const myCanvas = createCanvas(400,300);
 	//Set the parent of the canvas to an exisitng html element's id value 
 	myCanvas.parent("canvas-container");
 	background(220,40,50);

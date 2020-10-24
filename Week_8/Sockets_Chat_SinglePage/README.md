d2150d43d0adb90880364a07c8937e8ebd2b6510
Craig Protzel <craig.protzel@nyu.edu>
2020-10-24 13:42:29 -0700

    Added chat example

---
+++ Week_8/Sockets_Chat_SinglePage/README.md
@@ -0,0 +1,11 @@
+CHAT APP - Single Page
+----------------------
+
+### SETUP 
+* Run `npm install` to load the necessary node packages
+* Open two broswer windows both pointing to `localhost:3000`
+
+### NEXT STEPS
+* Save a user's name once at the beginning
+* Let other user's know when someone is typing
+
---
+++ Week_8/Sockets_Chat_SinglePage/index.js
@@ -0,0 +1,43 @@
+//Initialize the express 'app' object
+let express = require('express');
+let app = express();
+app.use('/', express.static('public'));
+
+//Initialize the actual HTTP server
+let http = require('http');
+let server = http.createServer(app);
+let port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log("Server listening at port: " + port);
+});
+
+//Initialize socket.io
+let io = require('socket.io').listen(server);
+
+//Listen for individual clients/users to connect
+io.sockets.on('connection', function(socket) {
+    console.log("We have a new client: " + socket.id);
+
+    //Listen for a message named 'msg' from this client
+    socket.on('msg', function(data) {
+        //Data can be numbers, strings, objects
+        console.log("Received a 'msg' event");
+        console.log(data);
+
+        //Send the data to all clients, including this one
+        io.sockets.emit('msg', data);
+
+        //Send the data to all other clients, not including this one
+        // socket.broadcast.emit('data', data);
+
+        //Send the data to just this client
+        // socket.emit('data', data);
+    });
+
+    //Listen for this client to disconnect
+    socket.on('disconnect', function() {
+        console.log("A client has disconnected: " + socket.id);
+    });
+});
+
+

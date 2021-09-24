496a2508fcfac270642704411f6dd5c57b9405b0
rutaitp <rk2906@nyu.edu>
2021-09-24 14:48:01 +0300

    fixed formatting

--- Week_04|JSON_p5_Github/p5_Canvas_Background/sketch.js
+++ Week_04|JSON_p5_Github/p5_Canvas_Background/sketch.js
@@ -1,13 +1,13 @@
 function setup() {
 	//Create the canvas and save it to a variable;
-	const myCanvas = createCanvas(window.innerWidth,window.innerHeight);
-	//Set the parent of the canvas to an exisitng html element's id value 
+	const myCanvas = createCanvas(window.innerWidth, window.innerHeight);
+	//Set the parent of the canvas to an exisitng html element's id value
 	myCanvas.parent("canvas-container");
-	background(220,40,50);
+	background(220, 40, 50);
 }
 
-function mouseMoved(){
+function mouseMoved() {
 	noStroke();
-	fill(255,255,255,20)
-    ellipse(mouseX,mouseY,70,70);
+	fill(255, 255, 255, 20)
+	ellipse(mouseX, mouseY, 70, 70);
 }

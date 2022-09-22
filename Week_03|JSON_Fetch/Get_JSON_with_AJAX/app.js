9529a6f445662ad14905f783352b207508cbd318
Mathura MG <mmg9291@gmail.com>
2022-09-22 21:26:16 +0530

    remove AJAX

--- Week_03|JSON_Fetch/Get_JSON_with_AJAX/app.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    console.log('page is loaded');
-    $.getJSON('http://api.open-notify.org/astros.json', function(data) {
-        console.log("Number of people in space : " + data.number);
-    })
-})

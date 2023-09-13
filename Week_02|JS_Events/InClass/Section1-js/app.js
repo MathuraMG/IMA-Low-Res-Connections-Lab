35dfab0f0b02950b47742153e0e650a9ccdf9454
Mathura MG <mmg9291@gmail.com>
2023-09-13 20:46:22 +0530

    add week 2 section 1 in class examples

---
+++ Week_02|JS_Events/InClass/Section1-js/app.js
@@ -0,0 +1,25 @@
+const batches = ["Avacado", "Banana", "Coconut", "Dragonfruit"];
+
+//ensure the page is loaded and all the HTML is availab;e
+window.addEventListener("load", function () { //callback function
+  console.log("page loaded");
+  //when user clicks button, show random name
+  //1 . ID the button
+  let randomButton = document.getElementById("random-button");
+
+  //2. listen for click
+  randomButton.addEventListener("click", function () {
+    console.log("button clicked");
+
+    //3. when clicked, give random number and name
+    let index = Math.floor(Math.random() * 4);
+    console.log(batches[index]);
+    document.getElementById("heading").innerHTML = batches[index];
+  })
+
+
+
+
+})
+
+console.log("hello");

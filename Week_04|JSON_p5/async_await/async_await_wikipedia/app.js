9931387953fba685e8e5138ad9496fa0cf4cc33f
rutaitp <rk2906@nyu.edu>
2024-09-23 12:54:34 +0300

    week 4 examples

---
+++ Week_04|JSON_p5/async_await/async_await_wikipedia/app.js
@@ -0,0 +1,59 @@
+console.log("Page is loading");
+
+//Make sure the window loads first
+window.addEventListener('load', () => {
+  console.log("The page has loaded");
+
+  async function getAstroData(){
+    //Send a request for data
+    let response = await fetch("http://api.open-notify.org/astros.json");
+    let data = await response.json();
+    //Do something with the data - add number to the page
+    let astronautNo = data.number;
+
+    //Create a p element that will show the number
+    let numberPar = document.createElement('p');
+    numberPar.innerHTML = astronautNo;
+
+    //Access the div container and append to it
+    let containerDiv = document.getElementById('data_container');
+    containerDiv.appendChild(numberPar);
+
+    //add styling to number paragraph
+    numberPar.setAttribute('class', 'astronaut_number');
+
+    //Also show names
+    let astronauts = data.people;
+    //As an alternative Promise.all syntax could also be used here
+    astronauts.forEach(async (element) => {
+      let astronautName = element.name;
+      //Replace space in astronaut name w/ %20
+      let astroSearch = astronautName.replace(' ', '%20');
+
+      //API url that we will ping
+      let astroUrl = 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + astroSearch;
+
+      let wikiResponse = await fetch(astroUrl);
+      let wikiData = await wikiResponse.json();
+      // console.log(wikiData);
+
+      //access url only
+      let url = wikiData[3][0];
+
+      //for every astronaut create a link
+      let astroLink = document.createElement('a');
+      astroLink.innerHTML = astronautName;
+
+      //set attributes
+      astroLink.setAttribute('href', url);
+      astroLink.setAttribute('target', '_blank');
+
+      //attach to the same div
+      containerDiv.appendChild(astroLink);
+
+      //add some styling
+      astroLink.setAttribute('class', 'astronaut_name');
+    })
+  }
+  getAstroData();
+});

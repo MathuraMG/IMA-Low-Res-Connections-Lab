328fd9b5b72faecb0f05e5d0dc7833aa223a2f47
rutaitp <rk2906@nyu.edu>
2024-10-18 13:29:06 +0300

    week 8 examples

---
+++ Week_08|Intro_Sockets/Sockets_Chat_SinglePage/public/app.js
@@ -0,0 +1,42 @@
+window.addEventListener('load', function () {
+
+    //Open and connect socket
+    let socket = io();
+    //Listen for confirmation of connection
+    socket.on('connect', function () {
+        console.log("Connected");
+    });
+
+    /* --- Code to RECEIVE a socket message from the server --- */
+    let chatBox = document.getElementById('chat-box-msgs');
+
+    //Listen for messages named 'msg' from the server
+    socket.on('msg', function (data) {
+        console.log("Message arrived!");
+        console.log(data);
+
+        //Create a message string and page element
+        let receivedMsg = data.name + ": " + data.msg;
+        let msgEl = document.createElement('p');
+        msgEl.innerHTML = receivedMsg;
+
+        //Add the element with the message to the page
+        chatBox.appendChild(msgEl);
+        //Add a bit of auto scroll for the chat box
+        chatBox.scrollTop = chatBox.scrollHeight;
+    });
+
+    /* --- Code to SEND a socket message to the Server --- */
+    let nameInput = document.getElementById('name-input')
+    let msgInput = document.getElementById('msg-input');
+    let sendButton = document.getElementById('send-button');
+
+    sendButton.addEventListener('click', function () {
+        let curName = nameInput.value;
+        let curMsg = msgInput.value;
+        let msgObj = { "name": curName, "msg": curMsg };
+
+        //Send the message object to the server
+        socket.emit('msg', msgObj);
+    });
+});

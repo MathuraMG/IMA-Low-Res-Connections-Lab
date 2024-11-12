cb3d26e3fab1d628ca064b5686787b4c68fa8106
Yuguang (YG) Zhang <37890050+viztopia@users.noreply.github.com>
2024-11-12 10:22:34 -0500

    Update sketch.js

--- Week_11|Client_Side_Libraries/ML5_Hello_World/sketch.js
+++ Week_11|Client_Side_Libraries/ML5_Hello_World/sketch.js
@@ -5,27 +5,21 @@ let classifier;
 let img;
 
 function preload() {
-    classifier = ml5.imageClassifier('MobileNet');
-    img = loadImage('images/cat.png');
+  classifier = ml5.imageClassifier('MobileNet');
+  img = loadImage('images/cat.png');
 }
 
 
 function setup() {
-    createCanvas(400, 400);
-    classifier.classify(img, gotResult);
-    image(img, 0, 0);
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0);
 }
 
-// A function to run when we get any errors and the results
-function gotResult(error, results) {
-    // Display error in the console
-    if (error) {
-      console.error(error);
-    } else {
-      // The results are in an array ordered by confidence.
-      console.log(results);
-      createDiv(`Label: ${results[0].label}`);
-      createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
-    }
-  }
-  
+// A function to run when we get the results
+function gotResult(results) {
+  // The results are in an array ordered by confidence.
+  console.log(results);
+  createDiv(`Label: ${results[0].label}`);
+  createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+}
